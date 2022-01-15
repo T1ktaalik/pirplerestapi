2d72f0de0677936f57fe1f5b38b5ef9b0577f61a
T1ktaalik <88627980+T1ktaalik@users.noreply.github.com>
2022-01-15 18:42:57 +0300

    iamchecking50
    iamchecking50

--- index.js
+++ index.js
@@ -1,80 +1,53 @@
-/*
-*a primary file for the api
-*
-*/
-
+/**a primary file for the api*/
 //Dependencies
 const http = require('http');
 const https = require('https');
 const url = require('url');
 const StringDecoder = require('string_decoder').StringDecoder;
-var config = require('./lib/config');
-var fs = require('fs');
+const fs = require('fs');
 //var _data = require('./lib/data');
+var config = require('./lib/config');
 var handlers = require('./lib/handlers');
 var helpers = require('./lib/helpers');
-
-//Testing 
-//@TODO delete this
-//_data.update('test', 'newFile'/*, {'fizz' : 'buzz'}*/, function(err){console.log('this was the error', err)});
-
-
-var httpServer = http.createServer(function(req, res){ unifiedServer(req, res)
-
-
-})
+var httpServer = http.createServer(function(req, res){ unifiedServer(req, res)})
 httpServer.listen(config.httpPort, console.log(`server is listening port ${config.httpPort} in ${config.envName} mode ...` ))
-
-
 //Instantiate the HTTPS server
 var httpsServerOptions = {
 'key': fs.readFileSync('./https/key.pem'), 
 'cert' : fs.readFileSync('./https/cert.pem')
 };
 var httpsServer = https.createServer(httpsServerOptions, function(req, res){ unifiedServer(req, res)})
-
 // Start the HTTPS server
 httpsServer.listen(config.httpsPort, console.log(`server is listening port ${config.httpsPort} in ${config.envName} mode ...` ))
-
-
-
-
-
 //Defining a request router
 const router = {
 'ping' : handlers.ping,
 'users' : handlers.users,
 };
-
 // All the server logic for both http and https server
 function unifiedServer(req, res) {
-    // get the url and parse interface
+    //get the url and parse interface
     var parsedUrl = url.parse(req.url, true);
     //get the path from url
     var path = parsedUrl.pathname;
     var trimmedPath = path.replace(/^\/+|\/+$/g, '');
-
-    //   get the query string as an object
+    //get the query string as an object
     var queryStringObject = parsedUrl.query;
-
     //Get the http method
     const method = req.method.toLowerCase();
-
     // Get the headers as an object
     const headers = req.headers;
-
     // Get the payload, if any 
     const decoder = new StringDecoder('utf-8');
     var buffer = '';
     req.on('data', function (data) {
-        buffer += decoder.write(data);
+    buffer += decoder.write(data);
     });
     req.on('end', function () {
         buffer += decoder.end();
-
-        // Choose the handler this request should go to. If one is not found, use the notFound handler
+        //Choose the handler this request should go to. If one is not found, use the notFound handler
         var chosenHandler = typeof (router[trimmedPath]) !== 'undefined' ? router[trimmedPath] : handlers.notFound;
-        // Construct the data object to send to the handler
+        //Construct the data object to send to the handler
         var data = {
             'trimmedPath': trimmedPath,
             'queryStringObject': queryStringObject,
@@ -82,7 +55,6 @@ function unifiedServer(req, res) {
             'headers': headers,
             'payload': helpers.parseJsonToObject(buffer),
         };
-
         //route the req to the handler specified in the router
         chosenHandler(data, (statusCode, payload) => {
             // Use the status code called back by the handler, or default to 200
